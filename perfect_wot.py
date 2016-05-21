d4e6af4bce20d6d3d0f17c5a3c5d5a8e707da559
inso <insomniak.fr@gmaiL.com>
2016-05-21 17:29:40 +0200

    Lot of optimizations

--- perfect_wot.py
+++ perfect_wot.py
@@ -1,60 +1,59 @@
 from wot_stories.fast_wot import WoT, plt
 from random import randint
 import numpy as np
+import logging
 
-if __name__ == '__main__':
+
+#@profile
+def run():
     NMAX = 1000000000
+    NB_TURN = 160*12
     wot = WoT(sig_period=0, sig_stock=45, sig_validity=12, sig_qty=3, xpercent=0.9, steps_max=4)
     wot.initialize(4)
-    individuals = [int(v) for v in wot.wot.vertices()]
+    individuals = [int(v) for v in wot.wot[0].vertices()]
     certs = {}
     for i in individuals:
         certs[i] = 0
-    nb_members = [3]
-    nb_identities = [3]
-
-    for i in range(0, 50):
-        print("Turn {0}".format(i))
-        next_individuals = individuals.copy()
-        for vertex in individuals:
-            others = individuals.copy()
-            del others[vertex]
-            if vertex in wot.history and i < wot.history[vertex][0] + 80*12:
-                if vertex in wot.wot.vertices() and vertex in wot.members:
-                    if 0 <= certs[vertex] < 45:
-                        if certs[vertex] % 9 == 0:
-                            new_id = wot.add_identity()
-                            wot.add_link(vertex, new_id)
-                            individuals.append(new_id)
-                            certs[new_id] = 0
-                            certs[vertex] += 1
-                        else:
-                            members_array = np.array([1 if k in wot.members else 2 for k in others])
-                            neighbours_array = np.array([2 if k in wot.wot.vertex(vertex).all_neighbours()
-                                                else 1 for k in others])
-                            need_certs_array = np.array([max(1, 5 - wot.next_wot.vertex(v).in_degree())
-                                                     for v in others])
-                            pond = (neighbours_array * members_array * need_certs_array) / \
-                                   np.sum(neighbours_array * members_array * need_certs_array)
-                            for i in range(0, 2):
-                                new_link_id = np.random.choice(others, p=pond)
-                                wot.add_link(vertex, new_link_id)
-                                certs[vertex] += 1
+    for i in range(0, NB_TURN):
+        need_certs_array = np.array([max(1, 5 - wot.wot[wot.turn].vertex(v).in_degree())
+                                     for v in wot.identities[wot.turn]])
+        members_array = np.array([1 if k in wot.members[wot.turn] else 2
+                                  for k in wot.identities[wot.turn]])
+        for vertex in wot.members[wot.turn]:
+            if vertex in wot.history and i < wot.history[vertex][0] + 80 * 12:
+                if 0 <= certs[vertex] < 45:
+                    if certs[vertex] % 9 == 0:
+                        new_id = wot.add_identity()
+                        wot.add_link(vertex, new_id)
+                        certs[new_id] = 0
+                        certs[vertex] += 1
                     else:
-                        print("No new cert for {0}".format(vertex))
+                        neighbours = wot.wot[wot.turn].vertex(vertex).all_neighbours()
+                        neighbours_array = np.array([2 if k in neighbours
+                                                     else 1 for k in wot.identities[wot.turn]])
+                        pond = (neighbours_array * members_array * need_certs_array) / \
+                               np.sum(neighbours_array * members_array * need_certs_array)
+                        for i in range(0, 2):
+                            new_link_id = np.random.choice(wot.identities[wot.turn], p=pond)
+                            wot.add_link(vertex, new_link_id)
+                            certs[vertex] += 1
+
+        print('\r[{0}{1}] {2:10.2f}% - Turn {3} : {4} members, {5} identities'.format('#' * int(wot.turn / NB_TURN * 10),
+                                       ' ' * (10 - (int(wot.turn / NB_TURN * 10))),
+                                       (wot.turn / NB_TURN) * 100,
+                                        wot.turn,
+                                        len(wot.members[wot.turn]),
+                                        len(wot.identities[wot.turn])))
         wot.next_turn()
-        nb_members.append(len(wot.members))
-        nb_identities.append(len(individuals))
-
-    fig, ax_f = plt.subplots()
-    ax_c = ax_f.twinx()
-    ax_f.plot(nb_members)
-    ax_f.plot(nb_identities)
-
-    ax_f.set_ylim(-5, max(max(nb_members), max(nb_identities)))
-    ax_f.set_xlim(-5, 4*40+5)
+    wot.end()
+    wot.save('perfect')
+#
+    wot.display_graphs()
     wot.draw()
-    wot.draw_turn(30)
-    #plt.savefig('perfect_shell.png', dpi=192, facecolor='w', edgecolor='w',
-    #            orientation='portrait')
+    #wot.draw_turn(10)
     plt.show()
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(levelname)s:%(module)s:%(funcName)s:%(message)s',
+                        level=logging.INFO)
+    run()
--- wot_stories/fast_wot.py
+++ wot_stories/fast_wot.py
@@ -4,7 +4,9 @@ from matplotlib import pyplot as plt
 from matplotlib import colors
 from mpl_toolkits.mplot3d import Axes3D
 from itertools import product
-
+import logging
+import pickle
+import os, errno
 
 class WoT:
     def __init__(self, sig_period, sig_stock, sig_validity, sig_qty, xpercent, steps_max):
@@ -24,11 +26,10 @@ class WoT:
         self.xpercent = xpercent
         self.steps_max = steps_max
 
-        self.wot = Graph(directed=True)
-        self.next_wot = Graph(directed=True)
-        self.wot.ep.time = self.wot.new_edge_property("int")
-        self.members = []
-        self.next_members = []
+        self.wot = {}
+        self.members = {}
+        self.identities = {}
+        self.received_links = []
 
         #Block number
         self.turn = 0
@@ -43,62 +44,96 @@ class WoT:
         self.color_iter = iter(colors.cnames.items())
         self.layouts = []
 
+    def save(self, dest):
+        try:
+            os.makedirs(dest)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+
     def initialize(self, nb_identities):
         """
         Initialize the Wot with first members (typically block 0)
         :param idties: List of pub_keys of identities (still not members)
         :param links: List of certifications ([issuer pub_key, certified pub_key], …)
         """
+        self.members[0] = []
+        self.identities[0] = []
 
-        identities = []
+        self.wot[0] = Graph(directed=True)
+        self.wot[0].ep.time = self.wot[0].new_edge_property("int")
         # Populate the graph with identities and certifications
         for idty in range(0, nb_identities):
-            print("{0} - Add identity during init".format(idty))
-            identities.append(int(self.wot.add_vertex()))
+            logging.debug("{0} - Add identity during init".format(idty))
+            v = self.wot[0].add_vertex()
+            logging.debug("{0} : New identity in the wot".format(int(v)))
+            # Keep track of memberships in time
+            if int(v) not in self.history:
+                self.history[int(v)] = [self.turn]
+                try:
+                    self.colors[int(v)] = next(self.color_iter)
+                except StopIteration:
+                    self.color_iter = iter(colors.cnames.items())
+                    self.colors[int(v)] = next(self.color_iter)
+            self.identities[self.turn].append(int(v))
 
-        init_links = list(product(identities, identities))
+        init_links = list(product(self.identities[0], self.identities[0]))
         for link in init_links:
             if link[1] != link[0]:
-                print("{0} -> {1} - Add certification during init".format(link[0], link[1]))
-                edge = self.wot.add_edge(link[0], link[1])
-                self.wot.ep.time[edge] = 0
+                logging.debug("{0} -> {1} - Add certification during init".format(link[0], link[1]))
+                edge = self.wot[0].add_edge(link[0], link[1])
+                self.wot[0].ep.time[edge] = 0
                 # Keep track of certifications for future analysis and plotting
                 self.past_links.append((0, link[0], link[1]))
 
         # Check if identities are members according to Wot rules
-        for vertex in self.wot.vertices():
+        for vertex in self.wot[0].vertices():
             enough_certs = vertex.in_degree() >= self.sig_qty
             if enough_certs:
-                print("{0} joined successfully on init".format(vertex))
-                self.members.append(int(vertex))
+                logging.debug("{0} joined successfully on init".format(vertex))
+                self.members[0].append(int(vertex))
 
                 # Keep track of memberships in time
                 if vertex not in self.history:
-                    self.history[int(vertex)] = []
                     self.colors[int(vertex)] = next(self.color_iter)
                 self.history[int(vertex)].append(self.turn)
             else:
-                print("Warning : {0} did not join during init ({1} certs)".format(int(vertex),
+                logging.debug("Warning : {0} did not join during init ({1} certs)".format(int(vertex),
                                                                                   vertex.in_degree()))
         self._prepare_next_turn()
 
+    #@profile
     def _prepare_next_turn(self):
         """
         Do a copy of the current state of the Wot
         """
-        self.next_wot = self.wot.copy()
-        self.next_members = self.members.copy()
+        self.received_links = []
+        self.wot[self.turn+1] = self.wot[self.turn].copy()
+        self.members[self.turn+1] = self.members[self.turn].copy()
+        self.identities[self.turn+1] = self.identities[self.turn].copy()
 
+    #@profile
     def add_identity(self):
         """
         Add an identity (still not member) to the graph
         :param idty: Public key of an individual
         :return:
         """
-        v = self.next_wot.add_vertex()
-        print("{0} : New identity in the wot".format(int(v)))
+        v = self.wot[self.turn].add_vertex()
+        logging.debug("{0} : New identity in the wot".format(int(v)))
+
+        # Keep track of memberships in time
+        if int(v) not in self.history:
+            self.history[int(v)] = [self.turn]
+            try:
+                self.colors[int(v)] = next(self.color_iter)
+            except StopIteration:
+                self.color_iter = iter(colors.cnames.items())
+                self.colors[int(v)] = next(self.color_iter)
+        self.identities[self.turn+1].append(int(v))
         return int(v)
 
+    #@profile
     def add_link(self, from_idty, to_idty):
         """
         Checks the validity of the certification and adds it in the graph if ok
@@ -106,39 +141,31 @@ class WoT:
         :param to_idty: Public key of the certified individual
         :return:
         """
+        if from_idty == to_idty:
+            logging.debug("{0} -> {1} : Error : link on self")
+
         # Checks the issuer signatures "stock"
-        vertex = self.next_wot.vertex(from_idty)
+        vertex = self.wot[self.turn+1].vertex(from_idty)
         out_links = vertex.out_edges()
         if vertex.out_degree() >= self.sig_stock:
-            print("{0} -> {1} : Too much certifications issued".format(from_idty, to_idty))
+            logging.debug("{0} -> {1} : Too much certifications issued".format(from_idty, to_idty))
             return
 
         # Checks if the issuer has waited enough time since his last certificate before emit a new one
-        if vertex.out_degree() > 0 and max([self.wot.ep.time[l] for l in out_links]) + self.sig_period > self.turn:
-            print("{0} -> {1} : Latest certification is too recent".format(from_idty, to_idty))
+        if vertex.out_degree() > 0 and max([self.wot[0].ep.time[l]
+                                            for l in out_links]) + self.sig_period > self.turn:
+            logging.debug("{0} -> {1} : Latest certification is too recent".format(from_idty, to_idty))
             return
 
         # Adds the certificate to the graph and keeps track
-        print("{0} -> {1} : Adding certification".format(from_idty, to_idty))
-        edge = self.next_wot.add_edge(from_idty, to_idty)
-        self.next_wot.ep.time[edge] = self.turn
+        logging.debug("{0} -> {1} : Adding certification".format(from_idty, to_idty))
+        edge = self.wot[self.turn+1].add_edge(from_idty, to_idty)
+        self.wot[self.turn+1].ep.time[edge] = self.turn
         self.past_links.append((self.turn, from_idty, to_idty))
 
         # Checks if the certified individual must join the wot as a member
-        if to_idty not in self.next_members and self.can_join(self.next_wot, to_idty):
-            print("{0} : Joining members".format(to_idty))
-
-            # Keep track of memberships in time
-            if to_idty not in self.history:
-                self.history[to_idty] = []
-                try:
-                    self.colors[to_idty] = next(self.color_iter)
-                except StopIteration:
-                    self.color_iter = iter(colors.cnames.items())
-                    self.colors[to_idty] = next(self.color_iter)
-
-            self.history[to_idty].append(self.turn)
-            self.next_members.append(to_idty)
+        if to_idty not in self.members[self.turn+1]:
+            self.received_links.append(to_idty)
 
     def ySentries(self, N):
         Y = {
@@ -153,7 +180,8 @@ class WoT:
                 return Y[k]
         return 0
 
-    def can_join(self, wot, idty):
+    #@profile
+    def can_join(self, wot, sentries, computed_links, distances, idty):
         """
         Checks if an individual must join the wot as a member regarding the wot rules
         Protocol 0.2
@@ -161,66 +189,90 @@ class WoT:
         :param idty:    Pubkey of the candidate
         :return: False or True
         """
-        sentries = [m for m in self.members if wot.vertex(m).out_degree() > self.ySentries(len(self.members))]
         # Extract the list of all connected members to idty at steps_max via certificates (edges)
 
         linked_in_range = []
         for s in sentries:
-            if graph_tool.topology.shortest_distance(wot,
-                                                 wot.vertex(s),
-                                                 wot.vertex(idty)) <= self.steps_max:
+            if distances[s][computed_links.index(idty)] <= self.steps_max:
                 linked_in_range.append(s)
 
         # Checks if idty is connected to at least xpercent of sentries
         enough_sentries = len(linked_in_range) >= len(sentries)*self.xpercent
         if not enough_sentries:
-            print("{0} : Cannot join : not enough sentries ({1}/{2})".format(idty,
+            logging.debug("{0} : Cannot join : not enough sentries ({1}/{2})".format(idty,
                                                                              len(linked_in_range),
                                                                              len(sentries)*self.xpercent))
 
         # Checks if idty has enough certificates to be a member
         enough_certs = wot.vertex(idty).in_degree() >= self.sig_qty
         if not enough_certs:
-            print("{0} : Cannot join : not enough certifications ({1}/{2}".format(idty,
+            logging.debug("{0} : Cannot join : not enough certifications ({1}/{2}".format(idty,
                                                                                   wot.vertex(idty).in_degree(),
                                                                                   self.sig_qty))
 
         return enough_certs and enough_sentries
 
+    #@profile
     def next_turn(self):
         """
         Updates the wot by removing expired links and members
         """
-        self.turn += 1
         dropped_links = []
-        print("== New turn {0} ==".format(self.turn))
-        tmp_wot = self.next_wot.copy()
+        logging.debug("== New turn {0} ==".format(self.turn+1))
+
+        tmp_wot = self.wot[self.turn+1].copy()
         # Links expirations
         for link in tmp_wot.edges():
             if self.turn > tmp_wot.ep.time[link] + self.sig_validity:
-                print("{0} -> {1} : Link expired ({2}/{3})".format(int(link.source()), int(link.target()),
-                                                                   self.turn,
+                logging.debug("{0} -> {1} : Link expired ({2}/{3})".format(int(link.source()), int(link.target()),
+                                                                   self.turn+1,
                                                                    tmp_wot.ep.time[link] + self.sig_validity))
-                self.next_wot.remove_edge(self.next_wot.edge(int(link.source()), int(link.target())))
-                dropped_links.append(link)
+                self.wot[self.turn+1].remove_edge(self.wot[self.turn+1].edge(int(link.source()), int(link.target())))
+                dropped_links.append(int(link.target()))
 
-        for link in dropped_links:
-            if int(link.target()) in self.next_members and not self.can_join(self.next_wot, int(link.target())):
-                print("{0} : Left community".format(link.target()))
-                self.next_members.remove(int(link.target()))
-                if int(link.target()) in self.history:
-                    self.history[int(link.target())].append(self.turn)
+        computed_links = dropped_links + self.received_links
 
-        self.wot = self.next_wot
-        self.members = self.next_members
-        #elf.layouts.append(graphviz_layout(self.wot, "twopi"))
+        sentries = [m for m in self.members[self.turn]
+                    if self.wot[self.turn].vertex(m).out_degree() > self.ySentries(len(self.members[self.turn]))]
+
+        distances = {}
+        for s in sentries:
+            distances[s] = graph_tool.topology.shortest_distance(self.wot[self.turn+1],
+                                              source=s,
+                                              target=computed_links,
+                                              max_dist=self.steps_max,
+                                              directed=True)
+
+        for receiver in self.received_links:
+            if receiver not in self.members[self.turn + 1] and self.can_join(self.wot[self.turn + 1],
+                                                                                sentries,
+                                                                                computed_links,
+                                                                               distances,
+                                                                             receiver):
+                logging.debug("{0} : Joined community".format(receiver))
+                self.history[receiver].append(self.turn)
+                self.members[self.turn+1].append(receiver)
+
+        for dropped in dropped_links:
+            if dropped in self.members[self.turn+1] and not self.can_join(self.wot[self.turn+1],
+                                                                                    sentries,
+                                                                                     computed_links,
+                                                                                    distances,
+                                                                                    dropped):
+                logging.debug("{0} : Left community".format(dropped))
+                self.members[self.turn+1].remove(dropped)
+                self.history[dropped].append(self.turn+1)
+
+        self.turn += 1
         self._prepare_next_turn()
 
-    def draw(self, zscale=1):
+    def end(self):
         for n in self.history:
-            if len(self.history[n]) % 2 != 0:
+            if len(self.history[n]) % 2 == 0:
                 self.history[n].append(self.turn)
-        pos = graph_tool.draw.arf_layout(self.wot)
+
+    def draw(self, zscale=1):
+        pos = graph_tool.draw.arf_layout(self.wot[self.turn])
 
         for n in self.history:
             periods = list(zip(self.history[n], self.history[n][1:]))
@@ -229,41 +281,55 @@ class WoT:
                 zline = linspace(p[0]*zscale, p[1]*zscale, nbpoints)
                 xline = linspace(pos[n][0], pos[n][0], nbpoints)
                 yline = linspace(pos[n][1], pos[n][1], nbpoints)
-                plot = self.ax.plot(xline, zline, yline, zdir='y', color=self.colors[n][0], alpha=1/(i % 2 + 1))
+                plot = self.ax.plot(xline, zline, yline, zdir='y',
+                                    color=self.colors[n][0], alpha=abs(1/(((i+1) % 2) + 1)))
 
         for link in self.past_links:
-            nbpoints = abs(pos[link[2]][0] - pos[link[1]][1])*zscale
+            nbpoints = abs(pos[link[2]][0] - pos[link[1]][1])*100
             zline = linspace(link[0]*zscale, link[0]*zscale, nbpoints)
             xline = linspace(pos[link[2]][0], pos[link[1]][0], nbpoints)
             yline = linspace(pos[link[2]][1], pos[link[1]][1], nbpoints)
             if link[1] in self.colors:
                 self.ax.plot(xline, zline, yline, zdir='y', color=self.colors[link[1]][0], alpha=0.1)
 
-        self.ax.set_xlim3d(0, 10)
-        self.ax.set_ylim3d(0, 10)
+        self.ax.set_xlim3d(min([pos[v][0] for v in self.wot[self.turn].vertices()]),
+                            max([pos[v][0] for v in self.wot[self.turn].vertices()]))
+        self.ax.set_ylim3d(min([pos[v][1] for v in self.wot[self.turn].vertices()]),
+                           max([pos[v][1] for v in self.wot[self.turn].vertices()]))
         self.ax.set_zlim3d(-5, (self.turn+1)*zscale)
 
     def draw_turn(self, turn):
-
         fig, ax_f = plt.subplots()
-        pos = graph_tool.draw.arf_layout(self.wot)
+        pos = graph_tool.draw.arf_layout(self.wot[self.turn])
 
+        for link in self.past_links:
+            if link[0] <= turn <= link[0] + self.sig_validity:
+                nbpoints = abs(pos[link[2]][0] - pos[link[1]][1])*100
+                xline = linspace(pos[link[2]][0], pos[link[1]][0], nbpoints)
+                yline = linspace(pos[link[2]][1], pos[link[1]][1], nbpoints)
+                ax_f.plot(xline, yline, color=self.colors[link[1]][0])
+
+        logging.debug(self.history)
         for n in self.history:
             periods = list(zip(self.history[n], self.history[n][1:]))
             for i, p in enumerate(periods):
-                if p[0] < turn < p[1]:
-                    nbpoints = abs(p[1] - p[0])
-                    xline = linspace(pos[n][0], pos[n][0], nbpoints)
-                    yline = linspace(pos[n][1], pos[n][1], nbpoints)
-                    ax_f.plot(xline, yline, color=self.colors[n][0], alpha=1 / (i % 2 + 1))
+                if p[0] <= turn <= p[1]:
+                    if i % 2 == 0:
+                        plt.scatter(pos[n][0], pos[n][1], color=self.colors[n][0])
+                    elif i % 2 == 1:
+                        plt.scatter(pos[n][0], pos[n][1], color=self.colors[n][0], alpha=0.5)
 
-        for link in self.past_links:
-            if link[0] < turn < link[0] + self.sig_validity:
-                nbpoints = abs(pos[link[2]][0] - pos[link[1]][1])
-                xline = linspace(pos[link[2]][0], pos[link[1]][0], nbpoints)
-                yline = linspace(pos[link[2]][1], pos[link[1]][1], nbpoints)
-                if link[1] in self.colors:
-                    ax_f.plot(xline, yline, color=self.colors[link[1]][0], alpha=0.1)
+        self.ax.set_xlim(min([pos[v][0] for v in self.wot[self.turn].vertices()]),
+                            max([pos[v][0] for v in self.wot[self.turn].vertices()]))
+        self.ax.set_ylim(min([pos[v][1] for v in self.wot[self.turn].vertices()]),
+                           max([pos[v][1] for v in self.wot[self.turn].vertices()]))
 
-        ax_f.set_xlim(-10, 10)
-        ax_f.set_ylim(-10, 10)
+    def display_graphs(self):
+        fig, ax_f = plt.subplots()
+        nb_members = [len(m) for m in self.members.values()]
+        nb_identities = [len(i) for i in self.identities.values()]
+        ax_f.plot(nb_members, color='blue')
+        ax_f.plot(nb_identities, color='green')
+
+        ax_f.set_ylim(-5, max(max(nb_members), max(nb_identities)) + 5)
+        ax_f.set_xlim(-5, self.turn + 5)
